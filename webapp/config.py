7d7a69ed67d6f89e4685c0afb24cfccdbb7506ce
Igor Novik <ig_novik@mail.ru>
2020-04-13 15:15:28 +0300

    11.03.2020

--- webapp/config.py
+++ webapp/config.py
@@ -12,3 +12,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db'
 SECRET_KEY = "guSU689_kerHSgv&sYW-5667_cP5mS91746*aw>83"
 
 REMEMBER_COOKIE_DURATION = timedelta(days=5)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False

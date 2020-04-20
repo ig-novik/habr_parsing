6d1bdae22189d2ffe6171312f2d886c28379645b
Igor Novik <ig_novik@mail.ru>
2020-04-20 20:50:00 +0300

    11.03.2020

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 
 from webapp.db import db
@@ -15,6 +16,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
     db.init_app(app)
+    migrate = Migrate(app, db)
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'user.login'
